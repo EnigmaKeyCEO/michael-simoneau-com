3ca8760e5d50158ce0e39af1e7a3f72a5ecd0304
Michael Simoneau <ceo@devcoup.com>
2025-10-27 04:19:00 -0400

    Add legacy redirects for Zero blog paths

---
+++ app/blog/zero-why.tsx
@@ -0,0 +1,5 @@
+import { Redirect } from "expo-router";
+
+export default function LegacyZeroWhyRedirect() {
+  return <Redirect href="/blog/zero" />;
+}
---
+++ app/blogs/[id].tsx
@@ -0,0 +1,9 @@
+import { Redirect, useLocalSearchParams } from "expo-router";
+
+export default function LegacyBlogsRedirect() {
+  const { id } = useLocalSearchParams<{ id?: string | string[] }>();
+  const normalizedId = Array.isArray(id) ? id[0] : id;
+  const href = normalizedId ? `/blog/${normalizedId}` : "/blog";
+
+  return <Redirect href={href} />;
+}
